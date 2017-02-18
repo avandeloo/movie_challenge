28d3a5c73377965d1b94656668f55814aa8fa438
avandeloo <anthony.vandeloo@gmail.com>
2017-02-17 21:50:48 -0600

    got starting and ending times working correctly when generating schedule

--- models/movie.rb
+++ models/movie.rb
@@ -28,9 +28,9 @@ class Movie
   end
 
   def expanded_time
-    total_minutes = (self.hours * 60) + self.minutes
-    run_time = (((total_minutes)/10.0).ceil) * 10 + 35
-    run_time = run_time.to_i * 60
+    total_seconds = (self.hours * 60 * 60) + (self.minutes * 60)
+    run_time = (((total_seconds)/10.0).ceil) * 10
+    run_time
   end
 
   def run_time
@@ -39,35 +39,31 @@ class Movie
 
   def gen_schedule
     @scheduled_events = []
-    start_times = []
-    end_times = []
 
     event_list = Events_List.new
-    time_left = event_list.check_time_left
-    closing = event_list.close_time
 
     movies_collection = event_list.create_events
+
     movies_collection.each do |movie_item|
       movie = Movie.new(movie_item)
+      start_times = []
+      end_times = []
       movie.format_time
       opening = event_list.open_time + (15 * 60)
       run_time = movie.expanded_time
+      starting = opening
+      time_left = event_list.check_time_left
 
       while time_left > run_time
 
-        starting = opening
-
-        ending_time = opening + run_time
+        ending = starting + run_time
 
         start_times << starting.strftime("%I:%M %p")
-        end_times << ending_time.strftime("%I:%M %p")
+        end_times << ending.strftime("%I:%M %p")
 
-        p starting.strftime("%I:%M %p")
-        p ending_time.strftime("%I:%M %p")
+        starting += run_time + (35 * 60)
 
-        opening += run_time + (35 * 60 * 60)
-
-        time_left -= run_time
+        time_left -= run_time + (35 * 60)
 
       end
         
